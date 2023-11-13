4fc0a93f5dd3701f34061558dfdf9aa071fadf66
charlyzan <carlos9720@live.com.mx>
2023-11-12 20:18:02 -0600

    idioma espanol

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,44 +1,35 @@
 import numpy as np
 import nltk
 nltk.download('punkt')
-from nltk.stem.porter import PorterStemmer
-stemmer = PorterStemmer()
+from nltk.stem import SnowballStemmer
 
+# Utilizamos SnowballStemmer para el idioma español
+stemmer = SnowballStemmer('spanish')
 
 def tokenize(sentence):
     """
-    split sentence into array of words/tokens
-    a token can be a word or punctuation character, or number
+    Divide la oración en una lista de palabras/tokens.
+    Un token puede ser una palabra, un carácter de puntuación o un número.
     """
-    return nltk.word_tokenize(sentence)
-
+    return nltk.word_tokenize(sentence, language='spanish')
 
 def stem(word):
     """
-    stemming = find the root form of the word
-    examples:
-    words = ["organize", "organizes", "organizing"]
-    words = [stem(w) for w in words]
-    -> ["organ", "organ", "organ"]
+    Aplica el stemming para encontrar la forma raíz de la palabra.
     """
     return stemmer.stem(word.lower())
 
-
 def bag_of_words(tokenized_sentence, words):
     """
-    return bag of words array:
-    1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
-    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    Retorna un arreglo de "bolsa de palabras":
+    1 para cada palabra conocida que existe en la oración, 0 en caso contrario.
     """
-    # stem each word
+    # Aplica stemming a cada palabra
     sentence_words = [stem(word) for word in tokenized_sentence]
-    # initialize bag with 0 for each word
+    # Inicializa la bolsa con 0 para cada palabra
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
-        if w in sentence_words: 
+        if w in sentence_words:
             bag[idx] = 1
 
     return bag
